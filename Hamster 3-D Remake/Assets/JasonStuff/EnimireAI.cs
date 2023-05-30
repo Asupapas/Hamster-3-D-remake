251e84102f9a235025b875e4cd0de7a7b0c2ed74
Kei <115186086+rrperc@users.noreply.github.com>
2023-05-30 13:52:36 -0700

    ENIMIRE done?

--- Hamster 3-D Remake/Assets/JasonStuff/EnimireAI.cs
+++ Hamster 3-D Remake/Assets/JasonStuff/EnimireAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemireAI : MonoBehaviour
+public class EnimireAI : MonoBehaviour
 {
     public NavMeshAgent agent;
 
