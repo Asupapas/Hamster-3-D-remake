da469d53c03972daccd44cac5df2a82e9ea625d0
Kei <115186086+rrperc@users.noreply.github.com>
2023-06-07 13:37:25 -0700

    Health UI updated Added Splatter

---
+++ Hamster 3-D Remake/Assets/JasonStuff/Jason Scripts/PlayerHP.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHP : MonoBehaviour
+{
+    [Header("Player Health amount")]
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    [Header("Add the Splatter image here")]
+    [SerializeField] private Image bloodSplatter = null;
+
+    [Header("Hurt Image Flash")]
+    [SerializeField] private Image hurtImage = null;
+    [SerializeField] private float hurtTimer = 0.1f;
+
+    [Header("Audio Name")]
+    [SerializeField] private AudioClip owAudio = null;
+    private AudioSource healthAudioSource = null;
+
+    public Healthbar healthbar;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+        healthAudioSource = GetComponent<AudioSource>();
+    }
+
+    void UpdateHealth()
+    {
+        Color splatterAlpha = bloodSplatter.color;
+        splatterAlpha.a = 1 - ((float)currentHealth / maxHealth);
+        bloodSplatter.color = splatterAlpha;
+    }
+
+    IEnumerator HurtFlash()
+    {
+        hurtImage.enabled = true;
+        healthAudioSource.PlayOneShot(owAudio);
+        yield return new WaitForSeconds(hurtTimer);
+        hurtImage.enabled = false;
+    }
+    IEnumerator DisableHurtImage()
+    {
+        yield return new WaitForSeconds(hurtTimer);
+        hurtImage.enabled = false;
+    }
+
+
+
+    void TakeDamage(int damage)
+    {
+        if (currentHealth > 0)
+        {
+            StartCoroutine(HurtFlash());
+            StartCoroutine(DisableHurtImage());
+            currentHealth -= damage;
+            healthbar.SetHealth(currentHealth);
+            UpdateHealth();
+        }
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene("LoserFU");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("DMG"))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+}
